44ce2c51ec6b88a69132c1d5ea6b05db31e14122
Nagahlon <c.genolhac@ludus-academie.com>
2022-05-05 10:19:57 +0200

    Mise à jour de la branche version1.0

---
+++ Cycle1_POO_CPP_2022/main.cpp
@@ -0,0 +1,13 @@
+//fichier main.cpp
+//version 1.0
+//Branch release
+//New pusch branch version1.0
+
+#include <stdio.h>
+
+int main() {
+
+	printf_s("version 1.0 du programme CPP\n");
+
+	return 0;
+}
