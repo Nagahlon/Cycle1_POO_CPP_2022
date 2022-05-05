414638909f3fec0d8dbc1c8cb970186a13320d3c
Nagahlon <c.genolhac@ludus-academie.com>
2022-05-05 15:23:56 +0200

    Introduction POO en C++

--- Cycle1_POO_CPP_2022/main.cpp
+++ Cycle1_POO_CPP_2022/main.cpp
@@ -4,11 +4,23 @@
 
 //New pusch branch version1.0
 
+
 #include <stdio.h>
+#include "TVect.h"
+
 
 int main() {
 
 	printf_s("version 1.0 du programme CPP\n");
+	printf_s("Introduction POO C++\n");
+
+	vecteur tvect;
+
+	tvect.n_x = 0;
+	tvect.n_y = 0;
+	tvect.n_z = 0;
+
+	initVecteur(&tvect, 5, 2, 9);
 
 	return 0;
 }
